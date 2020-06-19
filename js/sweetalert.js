97f74ac1962517061b09f9498f067129705de8a6
martinpcs21 <chuecosmartin@gmail.com>
2020-06-19 23:40:32 +0200

    First release commit
    Removed sweet alert
    Updated sitemap.txt
    Updated robots.txt
    Form for static websites included

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,7 +1,7 @@
-$('#SubmitMailES').click(function(){
-    swal("Mensaje enviado", "Le contestaremos cuanto antes", "success");
-}) 
+// $('#SubmitMailES').click(function(){
+//     swal("Mensaje enviado", "Le contestaremos cuanto antes", "success");
+// }) 
 
-$('#SubmitMailEN').click(function(){
-    swal("Message sent", "We will answer you as soon as possible", "success");
-})
+// $('#SubmitMailEN').click(function () {
+//     swal("Message sent", "We will answer you as soon as possible", "success");
+// })
